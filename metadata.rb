e0561395263d28dfb82e790683df69998a8ad9ca
Jordan Evans <jordane@osuosl.org>
2014-09-03 10:55:21 -0700

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,12 @@
+name             'celery'
+maintainer       'Oregon State University'
+maintainer_email 'chef@osuosl.org'
+license          'Apache 2.0'
+description      'Installs/Configures celery'
+long_description 'Installs/Configures celery'
+version          '0.1.0'
+
+supports         'centos', '~> 6.0'
+
+depends          'python'
+depends          'uwsgi'
