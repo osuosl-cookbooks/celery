440ada96b5806e9684ad5ae76ef1fbe6b79f7e37
Justin Noah <jnoah@osuosl.org>
2015-01-06 11:32:19 -0800

    version bump
    using bleeding edge poise

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@osuosl.org'
 license          'Apache 2.0'
 description      'Installs/Configures celery'
 long_description 'Installs/Configures celery'
-version          '0.1.1'
+version          '0.1.2'
 
 supports         'centos', '~> 6.0'
 
